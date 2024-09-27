0c3168c4646c42386e96489e7159f034deb76148
Ryan Naidoo <158072174+Ryan-004@users.noreply.github.com>
2024-09-27 21:43:52 +0200

    just changing the name of my instance

--- WardManagementSystem/WardManagementSystem/obj/Debug/net8.0/WardManagementSystem.AssemblyInfo.cs
+++ WardManagementSystem/WardManagementSystem/obj/Debug/net8.0/WardManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WardManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da4cb1ecef37a86890d5fe02b78c078b7b59d17c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed4cb8acd1f92790045436565b199ddae933899")]
 [assembly: System.Reflection.AssemblyProductAttribute("WardManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WardManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
