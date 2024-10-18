4048a1576715bd9935eb6b317d2e1610a7c0f312
Ryan Naidoo <158072174+Ryan-004@users.noreply.github.com>
2024-10-18 21:51:08 +0200

    Allowing for drop down list in admission add Ayden sub

---
+++ WardManagementSystem/WardManagementSystem.Data/Models/ViewModels/WardFullNameViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WardManagementSystem.Data.Models.ViewModels
+{
+    public class WardFullNameViewModel
+    {
+        public int WardId { get; set; }
+        public string WardName { get; set; }
+    }
+}
