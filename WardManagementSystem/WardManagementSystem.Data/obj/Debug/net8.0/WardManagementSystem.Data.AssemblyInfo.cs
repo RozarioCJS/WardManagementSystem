f912c1a7d426540b9c627edcf4512f11a8e8ca22
Ryan Naidoo <158072174+Ryan-004@users.noreply.github.com>
2024-10-18 17:48:57 +0200

    Added alert system

--- WardManagementSystem/WardManagementSystem.Data/obj/Debug/net8.0/WardManagementSystem.Data.AssemblyInfo.cs
+++ WardManagementSystem/WardManagementSystem.Data/obj/Debug/net8.0/WardManagementSystem.Data.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WardManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2cd7c51228e924c4a6bdf5c43ae301633e148cc")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec858319d10d2556aa04e94b607b60f6b1c45fe4")]
->>>>>>> facd682d39e9341b3e5db61403d021874ce4b688
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6dada5ba9ba5731bccc78d27a7a486a1f6137de")]
 [assembly: System.Reflection.AssemblyProductAttribute("WardManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WardManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
