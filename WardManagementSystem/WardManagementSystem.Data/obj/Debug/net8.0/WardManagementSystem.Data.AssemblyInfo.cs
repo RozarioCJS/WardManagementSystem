52966d1a76a54b4658708bc80b4dd752d1b5ee02
Rozario <sodienrozario@gmail.com>
2024-10-19 18:08:35 +0200

    delete confirm admin

--- WardManagementSystem/WardManagementSystem.Data/obj/Debug/net8.0/WardManagementSystem.Data.AssemblyInfo.cs
+++ WardManagementSystem/WardManagementSystem.Data/obj/Debug/net8.0/WardManagementSystem.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WardManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65086d9aeae8b13d8c6c1e52836cf024793ff9f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425157fe6b72401bbcc03e7b4698eaeaed896e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("WardManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WardManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
